48b711e67f083069e4fe27f1fe17c5c49b465adf
HazelP <antonio.pimentel.hazel@gmail.com>
2022-08-25 22:00:01 -0500

    Primer commit

---
+++ models/Testimoniales.js
@@ -0,0 +1,14 @@
+import Sequelize from "Sequelize";
+import db from '../config/db.js'
+
+export const Testimonial = db.define('testimoniales', {
+    nombre: {
+        type: Sequelize.STRING
+    },
+    correo: {
+        type: Sequelize.STRING
+    },
+    mensaje: {
+        type: Sequelize.STRING
+    }
+});
